892fe164ec4dbaa87e234be53304d6a688dcaf75
FakePickle <harsh22200@iiitd.ac.in>
2024-09-03 01:54:11 +0530

    feat: Added my updated qtile configs with dunst and picom integrated

---
+++ Configs/qtile/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# -----------------------------------------
+#           NITROGEN AND PICOM
+# -----------------------------------------
+
+autorandr --load legion_hdmi
+nitrogen --restore &
+picom -b 
