491704c758805977d91c2ff71b18231daa6a78a3
FakePickle <harsh22200@iiitd.ac.in>
2025-05-09 18:03:39 +0530

    refactor: remove unused plugins

--- .config/nvim/lua/fakepickle/lazy/copilot.lua
+++ .config/nvim/lua/fakepickle/lazy/copilot.lua
@@ -1,36 +1,11 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        event = "VimEnter",
-        config = function()
-            require("copilot").setup({
-                suggestion = {
-                    auto_trigger = true,
-                    enabled = true,
-                    hide_during_completion = true,
-                },
-                filetypes = {
-                    markdown = true,
-                    python = true,
-                    rust = true,
-                    lua = true,
-                    tex = true,
-                    c = true,
-                    cpp = true,
-                    go = true,
-                    javascript = true,
-                    typescript = true,
-                    html = true,
-                    css = true,
-                    scss = true,
-                    json = true,
-                    yaml = true,
-                    toml = true,
-                    shell = true,
-                },
-                panel = { enabled = true },
-            })
-        end,
-    }
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
 }
 
